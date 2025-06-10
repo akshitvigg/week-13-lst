e3740c72fcc49705db986f18546528f03d665c0d
Akshit Vig <akshitvig48@gmail.com>
2025-06-11 01:30:18 +0530

    ksajdkf

--- dist/address.js
+++ dist/address.js
@@ -1,11 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TOKEN_MINT_ADDRESS = exports.PUBLIC_KEY = exports.PRIVATE_KEY = void 0;
-exports.PRIVATE_KEY = [
-    185, 47, 152, 61, 225, 215, 81, 8, 188, 89, 230, 148, 34, 85, 48, 109, 114,
-    19, 119, 77, 69, 1, 242, 216, 137, 37, 21, 221, 217, 48, 229, 236, 8, 219,
-    200, 47, 94, 102, 133, 136, 132, 174, 45, 69, 144, 181, 44, 130, 58, 11, 95,
-    115, 224, 217, 68, 80, 160, 88, 248, 173, 60, 20, 27, 237,
-];
+exports.PRIVATE_KEY = "";
 exports.PUBLIC_KEY = "badzog4uMZdEJmkAJrzUSbp4Yzvp79ax9b3ozKxmFvc";
-exports.TOKEN_MINT_ADDRESS = "mntGEfeCSUjfAiN3vyGCRkQ1ysG5H4zcpirjGdDDYCr";
+exports.TOKEN_MINT_ADDRESS = "";
